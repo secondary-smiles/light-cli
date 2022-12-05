3f8f1a7cec83ea3348a114ba450b687682cf778c
secondary-smiles <shavkinder@gmail.com>
2022-12-05 13:16:21 -0500

    Add generate urls from provided source

--- src/globals.ts
+++ src/globals.ts
@@ -1,7 +1,8 @@
-import {bold, green, gray, brightGreen} from "fmt/colors.ts";
+import {bold, gray, brightGreen} from "fmt/colors.ts";
 
 export const VERSION = "0.0.1";
 export const NAME = "light";
 export const ABOUT = "Placeholder about text";
 export const PREFIX = '+';
-export const USAGE = `${(NAME)} [${bold("FLAGS")}] ${brightGreen(PREFIX)}<${bold("SOURCE")}> <${gray("SOURCE PROGRAM")}> [${gray("SOURCE PROGRAM FLAGS")}]`;
+export const USAGE  = `${(NAME)} [${bold("FLAGS")}] ${brightGreen(PREFIX)}<${bold("SOURCE")}> <${gray("SOURCE PROGRAM")}> [${gray("SOURCE PROGRAM FLAGS")}]`;
+export const ACTION = "action.toml";
--- src/lib/program/source.ts
+++ src/lib/program/source.ts
@@ -1,13 +1,56 @@
 import { ProgramArgs } from "../cli/parseArgs.ts";
 import { info } from "../util/info.ts";
+import { error } from "../util/error.ts";
+import { ACTION, NAME } from "../../globals.ts";
+
+interface UrlGroup {
+  preUrl?: URL;
+  postUrl?: URL;
+}
 
 function resolveSource(args: ProgramArgs) {
   getActionFile(args.source);
 }
 
+// CHECKS:
+// light.{{source}}/action.toml
+// {{source}}/light/action.toml
 async function getActionFile(source: string) {
-  const url = new URL(`https://${source}/light/action.toml`);
-  info.log(await fetch(url));
+  const url = getUrl(source);
+
+  info.info(url);
+}
+
+function getUrl(source: string) {
+  if (source.split(".").length < 2) {
+    error(new Error(`${source} could not be parsed as URL`));
+  }
+
+  const postSourceUrlString = `https://${source}/${NAME}/${ACTION}`;
+  const preSourceUrlString = `https://${NAME}.${source}/${ACTION}`;
+  const postUrl = checkDomain(preSourceUrlString);
+  const preUrl = checkDomain(postSourceUrlString);
+
+  if (!preUrl && !postUrl) {
+    error(new Error(`${source} could not be parsed as URL`));
+  }
+
+  const urlGroup: UrlGroup = {
+    preUrl: preUrl,
+    postUrl: postUrl,
+  };
+
+  return urlGroup;
+}
+
+function checkDomain(source: string) {
+  let url: URL | undefined;
+  try {
+    url = new URL(source);
+  } catch (e) {
+    info.warn(e.message);
+  }
+  return url;
 }
 
 export { resolveSource };
