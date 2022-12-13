d8b19b89005f45e380bc5bf81d06f6a7ba27c192
secondary-smiles <shavkinder@gmail.com>
2022-12-13 13:15:32 -0500

    Implement loading and clearing with logger

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import { logger } from "logger";
 
 async function main() {
   logger.log("hello, world!");
-  logger.notice("this is a notice about a feature")
+  logger.notice("this is a notice");
   logger.warn("this is a warning");
+
+  logger.load("loading for a second");
+  await new Promise((r) => setTimeout(r, 1000));
   throw new Problem("and this is a thrown error");
 }
 
--- src/lib/util/logging/logger.ts
+++ src/lib/util/logging/logger.ts
@@ -1,27 +1,107 @@
-import { bold, brightYellow, red, blue } from "fmt/colors.ts";
+import { bold, brightYellow, red, blue, gray } from "fmt/colors.ts";
+
+interface LoaderData {
+  loading: boolean;
+  icons: string[];
+  index: number;
+  currentLoader: number;
+  message: string;
+}
+
+const escapeCodes = {
+  clearLine: `\r\x1b[K`,
+  lineUp: `\x1b[1F`,
+
+  stderrWrite(message: string) {
+    Deno.stderr.writeSync(new TextEncoder().encode(message));
+  },
+
+  stdoutWrite(message: string) {
+    Deno.stdout.writeSync(new TextEncoder().encode(message));
+  },
+};
 
 class Logger {
   prefix: string;
+  private loaderData: LoaderData;
 
   constructor(prefix = "") {
-    this.prefix = bold(prefix)
+    this.prefix = bold(prefix);
+
+    this.loaderData = {
+      loading: false,
+      currentLoader: NaN,
+      icons: ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"],
+      index: 0,
+      message: "",
+    };
   }
 
   log(message: string) {
+    this.stopLoad();
+
     //TODO: Check verbosity
     console.log(message);
+    this.load();
   }
 
   notice(message: string) {
+    this.stopLoad();
+
     console.info(blue(this.prefix + message));
+    this.load();
   }
 
   warn(message: string) {
+    this.stopLoad();
+
     console.warn(brightYellow(this.prefix + message));
+    this.load();
   }
 
   error(message: string) {
+    this.stopLoad();
+
     console.error(red(this.prefix + message));
+    this.load();
+  }
+
+  load(message = this.loaderData.message, speed = 50) {
+    this.stopLoad();
+
+    this.loaderData.loading = true;
+    this.loaderData.message = message;
+    this.loaderData.currentLoader = setInterval(() => {
+      this.processLoad();
+    }, speed);
+  }
+
+  stopLoad() {
+    if (this.loaderData.loading) {
+      escapeCodes.stderrWrite(escapeCodes.clearLine);
+      clearInterval(this.loaderData.currentLoader);
+      this.loaderData.loading = false;
+    }
+  }
+
+  clear(lines: number) {
+    this.stopLoad();
+
+    for (let i = 0; i < lines; i++) {
+      escapeCodes.stdoutWrite(escapeCodes.clearLine);
+      escapeCodes.stdoutWrite(escapeCodes.lineUp);
+    }
+  }
+
+  private processLoad() {
+    const icon = this.loaderData.icons[this.loaderData.index];
+    const message = `${escapeCodes.clearLine}${icon} ${gray(
+      `${this.loaderData.message}..`
+    )}`;
+
+    escapeCodes.stderrWrite(message);
+    this.loaderData.index =
+      (this.loaderData.index + 1) % this.loaderData.icons.length;
   }
 }
 
