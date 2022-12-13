1d5f87dc1b4e0929edb0ae60502558ea5db8efe8
secondary-smiles <shavkinder@gmail.com>
2022-12-13 13:22:05 -0500

    Add allCommands and Command types

---
+++ src/lib/cli/commands/mod.ts
@@ -0,0 +1,9 @@
+
+
+private const _allCommands = {};
+
+function allCommands() {
+  return _allCommands;
+}
+
+export { allCommands };
---
+++ src/lib/cli/types.ts
@@ -0,0 +1,19 @@
+interface Arg {
+  name: string;
+  short: string;
+  long: string;
+  help: string;
+}
+
+class Command {
+  arg: Arg;
+
+  run: Function;
+
+  constructor(arg: Arg, run: Function) {
+    this.arg = arg;
+    this.run = run;
+  }
+}
+
+export type { Arg, Command };
