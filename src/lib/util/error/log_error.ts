55e10c09091feeb04fc9a3dba30029ba9dfc3653
secondary-smiles <shavkinder@gmail.com>
2022-12-16 23:03:48 -0500

    Add verbosity to error log

--- src/lib/util/error/log_error.ts
+++ src/lib/util/error/log_error.ts
@@ -2,12 +2,19 @@ import { gray } from "fmt/colors.ts";
 
 import { Problem } from "error";
 import { logger } from "logger";
+import { globals } from "globals";
 
 function log_error(reason: Problem) {
-  // TODO: Log {reason}
+  if (globals.command.verbose) {
+    throw reason;
+  }
   const message = `${reason.message} ${gray(`(${reason.code})`)}`;
   logger.error(message);
 
+  // Normal Error was passed without code
+  if (!reason.code) {
+    reason.code = 1;
+  }
   Deno.exit(reason.code);
 }
 
