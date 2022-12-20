5d63ccc23efbdf5c65ab5841c275e8e741dacafc
secondary-smiles <shavkinder@gmail.com>
2022-12-20 12:31:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,36 @@
 mkdir -p "$HOME"/.light/light
+PATH_FOLDER="$HOME"/.light/light/bin
+mkdir -p "$PATH_FOLDER"
 git clone https://github.com/secondary-smiles/light-core.git "$HOME"/.light/light/light-core
+
+PATH_EXPORT="export PATH="$PATH_FOLDER:'$PATH'
+
+case "$(basename "$SHELL")" in
+bash)
+  HOME_FILE="$HOME"/.bashrc
+
+  if grep -q "$PATH_EXPORT" "$HOME_FILE"; then
+    # Do nothing
+    :
+  else
+    echo "$PATH_EXPORT" >>"$HOME_FILE"
+  fi
+  ;;
+zsh)
+  HOME_FILE="$HOME"/Desktop/test.txt
+
+  if grep -Fqx "$PATH_EXPORT" "$HOME_FILE"; then
+    # Do nothing
+    : ;
+  else
+    echo "$PATH_EXPORT" >>"$HOME_FILE" ;
+  fi
+  ;;
+*)
+  echo "Couldn't determine shell init file."
+  echo "$PATH_FOLDER} should be added to your '\$PATH' variable."
+  echo "For example: $PATH_EXPORT"
+  ;;
+esac
 
 # TODO: compile & install program binary
